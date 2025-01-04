c846f19fa30512f292e3150a68f3d44059671824
sandratraniaina <sandratrarafa@gmail.com>
2025-01-04 22:32:44 +0300

    refactor: Refactor controller to use redirect method

--- src/main/java/mg/sandratra/bakery/controller/HomeController.java
+++ src/main/java/mg/sandratra/bakery/controller/HomeController.java
@@ -5,23 +5,27 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import mg.sandratra.bakery.util.form.Option;
+import mg.sandratra.bakery.util.form.Redirection;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String redirectToIndex(Model model) {
-        model.addAttribute("content", "index");
+    public ModelAndView redirectToIndex() {
+        ModelAndView model = new ModelAndView();
 
-        model.addAttribute("options", List.of(
+        model.addObject("content", "index");
+
+        model.addObject("options", List.of(
             new Option("1", "This is option 1"),
             new Option("2", "This is option 2"),
             new Option("3", "This is option 3")
         ));
 
-        return "layout";
+        return Redirection.redirect(model, "index");
     }
 
     @GetMapping("/login")
