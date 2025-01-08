eb3bd3eb525051c89100a72c25f384ac4fe6b556
sandratraniaina <sandratrarafa@gmail.com>
2025-01-08 05:51:03 +0300

    feat: Create Recipe model

---
+++ src/main/java/mg/sandratra/bakery/models/recipe/Recipe.java
@@ -0,0 +1,17 @@
+package mg.sandratra.bakery.models.recipe;
+
+import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Recipe {
+    private int id;
+    private String name;
+    private String description;
+    private Timestamp createdAt;
+}
