2e993a33f76c6b048a5d5e4eac36cd28d2950c81
sandratraniaina <sandratrarafa@gmail.com>
2025-01-08 20:14:56 +0300

    feat: Create mapToModel and mapToDto method for recipe ingreidne service

--- src/main/java/mg/sandratra/bakery/services/recipe/RecipeIngredientService.java
+++ src/main/java/mg/sandratra/bakery/services/recipe/RecipeIngredientService.java
@@ -1,7 +1,10 @@
 package mg.sandratra.bakery.services.recipe;
 
+import mg.sandratra.bakery.dto.recipe.RecipeIngredientDto;
+import mg.sandratra.bakery.models.ingredient.Ingredient;
 import mg.sandratra.bakery.models.recipe.RecipeIngredient;
 import mg.sandratra.bakery.repository.recipe.RecipeIngredientRepository;
+import mg.sandratra.bakery.services.IngredientService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +15,8 @@ import java.util.List;
 public class RecipeIngredientService {
 
     private final RecipeIngredientRepository recipeIngredientDao;
+
+    private final IngredientService ingredientService;
 
     public List<RecipeIngredient> findAll() {
         return recipeIngredientDao.findAll();
@@ -35,5 +40,31 @@ public class RecipeIngredientService {
 
     public int deleteById(Long recipeId, Long ingredientId) {
         return recipeIngredientDao.deleteById(recipeId, ingredientId);
+    }
+
+    public RecipeIngredientDto mapToDto(RecipeIngredient recipeIngredient) {
+        if (recipeIngredient == null) {
+            return null;
+        }
+
+        Ingredient ingredient = ingredientService.findById(recipeIngredient.getIngredientId());
+        if (ingredient == null) {
+            throw new IllegalArgumentException("Ingredient not found for ID: " + recipeIngredient.getIngredientId());
+        }
+
+        return new RecipeIngredientDto(recipeIngredient.getQuantity(), ingredient);
+    }
+
+    public RecipeIngredient mapToModel(RecipeIngredientDto dto, Long recipeId) {
+        if (dto == null || dto.getIngredient() == null) {
+            return null;
+        }
+
+        Long ingredientId = dto.getIngredient().getId();
+        if (ingredientId == null) {
+            throw new IllegalArgumentException("Ingredient ID cannot be null.");
+        }
+
+        return new RecipeIngredient(recipeId, ingredientId, dto.getQuantity());
     }
 }
