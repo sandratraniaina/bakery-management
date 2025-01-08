b4c5a50557ff423f1bd75d783d369d9859904232
sandratraniaina <sandratrarafa@gmail.com>
2025-01-08 21:18:44 +0300

    feat: Create getIngredientName method

--- src/main/java/mg/sandratra/bakery/dto/recipe/RecipeDto.java
+++ src/main/java/mg/sandratra/bakery/dto/recipe/RecipeDto.java
@@ -2,6 +2,7 @@ package mg.sandratra.bakery.dto.recipe;
 
 import java.sql.Timestamp;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,4 +17,17 @@ public class RecipeDto {
     private String description;
     private List<RecipeIngredientDto> recipeIngredients;
     private Timestamp createdAt;
+
+    public String getIngredientNames() {
+        // Check if the list is empty or null
+        if (recipeIngredients == null || recipeIngredients.isEmpty()) {
+            return "";
+        }
+
+        // Use stream to map the RecipeIngredientDto to the ingredient name and join
+        // them with commas
+        return recipeIngredients.stream()
+                .map(recipeIngredientDto -> recipeIngredientDto.getIngredient().getName()) // Get ingredient name
+                .collect(Collectors.joining(", ")); // Join names with commas
+    }
 }
