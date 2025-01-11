dd7f2167c7b1f94eb88d95f0f8e34372e0380a4b
sandratraniaina <sandratrarafa@gmail.com>
2025-01-11 18:24:55 +0300

    chore: Remove unused import

--- src/main/java/mg/sandratra/bakery/repository/ingredient/IngredientRepository.java
+++ src/main/java/mg/sandratra/bakery/repository/ingredient/IngredientRepository.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
-import org.springframework.data.repository.query.parser.Part.IgnoreCaseType;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
