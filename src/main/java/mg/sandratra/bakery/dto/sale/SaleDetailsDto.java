f546b78c1f99416ed4e52e23423d6f38e71eb0cb
sandratraniaina <sandratrarafa@gmail.com>
2025-01-11 21:25:41 +0300

    feat: Create SaleDetailsDto dto

---
+++ src/main/java/mg/sandratra/bakery/dto/sale/SaleDetailsDto.java
@@ -0,0 +1,21 @@
+package mg.sandratra.bakery.dto.sale;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import mg.sandratra.bakery.models.product.Product;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SaleDetailsDto {
+    private Long id;
+    private Product product;
+    private int quantity;
+    private BigDecimal unitPrice;
+    private BigDecimal subTotal;
+    private Timestamp createdAt;
+}
