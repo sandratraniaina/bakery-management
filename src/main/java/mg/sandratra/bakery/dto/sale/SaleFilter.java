28ff4e7d420e7c6bc85090809429b9c3c994d9a1
sandratraniaina <sandratrarafa@gmail.com>
2025-01-12 16:30:47 +0300

    feat: Conver Boolean type to BooleanValue

--- src/main/java/mg/sandratra/bakery/dto/sale/SaleFilter.java
+++ src/main/java/mg/sandratra/bakery/dto/sale/SaleFilter.java
@@ -4,36 +4,35 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import mg.sandratra.bakery.enums.ProductType;
+import mg.sandratra.bakery.utils.filter.BooleanValue;
 import mg.sandratra.bakery.utils.filter.Filter;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class SaleFilter implements Filter {
-    private boolean isNature;
+    private BooleanValue nature;
     private ProductType productType;
 
     @Override
     public String buildQuery() {
         StringBuilder query = new StringBuilder();
 
-        query.append("SELECT * FROM v_breadmaking_product_nature bmpn ");
+        query.append("SELECT * FROM v_sale_product_nature spn ");
 
-        if (productType != null || isNature) {
+        if (productType != null) {
             query.append("WHERE 1=1 ");
 
             if (productType != null) {
-                query.append("AND bmpn.product_type = '")
+                query.append("AND spn.product_type = '")
                         .append(productType.name())
                         .append("' ");
             }
 
-            if (isNature) {
-                query.append("AND bmpn.is_nature = true ");
+            if (nature.getValue() != null) {
+                query.append("AND spn.is_nature = true ");
             }
         }
-
-        query.append("ORDER BY bmpn.production_date, bmpn.id");
 
         return query.toString();
     }
