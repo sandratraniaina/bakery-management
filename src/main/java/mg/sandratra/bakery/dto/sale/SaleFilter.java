c742aeebab20d30f64b41380f384431646ab6d14
sandratraniaina <sandratrarafa@gmail.com>
2025-01-12 15:31:30 +0300

    feat: Create SaleFilter filter

---
+++ src/main/java/mg/sandratra/bakery/dto/sale/SaleFilter.java
@@ -0,0 +1,33 @@
+package mg.sandratra.bakery.dto.sale;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import mg.sandratra.bakery.enums.ProductType;
+import mg.sandratra.bakery.utils.filter.Filter;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SaleFilter implements Filter {
+    private boolean isNature;
+    private ProductType productType;
+
+    @Override
+    public String buildQuery() {
+        StringBuilder query = new StringBuilder();
+
+        query.append("SELECT * FROM v_breadmaking_product_nature bmpn ")
+                .append("WHERE bmpn.is_nature = ").append(isNature).append(" ");
+
+        if (productType != null) {
+            query.append("AND bmpn.product_type = '")
+                    .append(productType.name())
+                    .append("' ");
+        }
+
+        query.append("ORDER BY bmpn.production_date, bmpn.id");
+
+        return query.toString();
+    }
+}
