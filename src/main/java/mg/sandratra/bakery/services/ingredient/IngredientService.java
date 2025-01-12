5a54467043671bdf842f6979b042a22eb4803f9f
sandratraniaina <sandratrarafa@gmail.com>
2025-01-12 22:37:44 +0300

    refactor: Move to services.ingredient package

---
+++ src/main/java/mg/sandratra/bakery/services/ingredient/IngredientService.java
@@ -0,0 +1,35 @@
+package mg.sandratra.bakery.services.ingredient;
+
+import mg.sandratra.bakery.models.ingredient.Ingredient;
+import mg.sandratra.bakery.repository.ingredient.IngredientRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class IngredientService {
+
+    private final IngredientRepository ingredientDao;
+
+    public List<Ingredient> findAll() {
+        return ingredientDao.findAll();
+    }
+
+    public Ingredient findById(Long id) {
+        return ingredientDao.findById(id);
+    }
+
+    public int saveOrUpdate(Ingredient ingredient) {
+        if (ingredient.getId() == null) {
+            return ingredientDao.save(ingredient);
+        } else {
+            return ingredientDao.update(ingredient);
+        }
+    }
+
+    public int deleteById(Long id) {
+        return ingredientDao.deleteById(id);
+    }
+}
