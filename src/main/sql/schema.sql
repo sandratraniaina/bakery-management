b74500ff32a6ad5b76d01bebb0e7b607de3635a3
sandratraniaina <sandratrarafa@gmail.com>
2025-01-14 14:27:15 +0300

    feat: Add product_recommendations table

--- src/main/sql/schema.sql
+++ src/main/sql/schema.sql
@@ -108,6 +108,13 @@ CREATE TABLE product_movement(
   CONSTRAINT product_movement_pkey PRIMARY KEY(id)
 );
 
+CREATE TABLE product_redommendations(
+  id integer NOT NULL,
+  product_id integer NOT NULL,
+  created_at date NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT product_redommendations_pkey PRIMARY KEY(id)
+);
+
 CREATE TABLE recipe(
   id serial NOT NULL,
   "name" varchar(100) NOT NULL,
@@ -198,6 +205,10 @@ ALTER TABLE sale_details
 ALTER TABLE product_movement
   ADD CONSTRAINT product_movement_product_id_fkey
     FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE Set null;
+
+ALTER TABLE product_redommendations
+  ADD CONSTRAINT product_redommendations_product_id_fkey
+    FOREIGN KEY (product_id) REFERENCES product (id);
 
 CREATE OR REPLACE FUNCTION update_ingredient_stock_on_movement()
 RETURNS TRIGGER AS $$ 
