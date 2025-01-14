966a02509fb8ee2bf878add8738be3e44260f081
sandratraniaina <sandratrarafa@gmail.com>
2025-01-14 15:02:53 +0300

    feat: Create utility method

--- src/main/java/mg/sandratra/bakery/dto/product/ProductRecommendationsDto.java
+++ src/main/java/mg/sandratra/bakery/dto/product/ProductRecommendationsDto.java
@@ -1,11 +1,13 @@
 package mg.sandratra.bakery.dto.product;
 
 import java.sql.Date;
+import java.util.Locale;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import mg.sandratra.bakery.models.product.Product;
+import mg.sandratra.bakery.utils.DateUtils;
 
 @Data
 @AllArgsConstructor
@@ -14,4 +16,16 @@ public class ProductRecommendationsDto {
     private Long id;
     private Date createdAt;
     private Product product;
+
+    public String getMonth() {
+        return DateUtils.getMonthInText(createdAt, Locale.ENGLISH);
+    }
+
+    public int getYear() {
+        return createdAt.toLocalDate().getYear();
+    }
+
+    public String getProductName() {
+        return product.getName();
+    }
 }
