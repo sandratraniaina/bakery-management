1707bc12aee71361bcecbea6d6f4cf7f986f35c7
sandratraniaina <sandratrarafa@gmail.com>
2025-01-21 05:00:32 +0300

    feat: Update CustomerActivitiesControleller and client-list

--- src/main/java/mg/sandratra/bakery/controllers/sale/CustomerActivitiesController.java
+++ src/main/java/mg/sandratra/bakery/controllers/sale/CustomerActivitiesController.java
@@ -3,13 +3,13 @@ package mg.sandratra.bakery.controllers.sale;
 import mg.sandratra.bakery.controllers.BaseController;
 import mg.sandratra.bakery.dto.sale.SaleDto;
 import mg.sandratra.bakery.services.sale.SaleService;
+import mg.sandratra.bakery.services.sale.filter.SaleCustomerFilter;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import lombok.RequiredArgsConstructor;
 
-import java.sql.Date;
 import java.util.List;
 
 @Controller
@@ -21,11 +21,11 @@ public class CustomerActivitiesController extends BaseController {
     private final SaleService saleService;
 
     @GetMapping
-    public ModelAndView getAllSalesClient(@RequestParam(value = "date", required = false) Date date) {
-        List<SaleDto> sales = saleService.findByDate(date).stream().map(saleService::mapToDto).toList();
+    public ModelAndView getAllSalesClient(@ModelAttribute SaleCustomerFilter customerFilter) {
+        List<SaleDto> sales = saleService.search(customerFilter).stream().map(saleService::mapToDto).toList();
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("sales", sales);
-        modelAndView.addObject("date", date);
+        modelAndView.addObject("filter", customerFilter);
         return redirect(modelAndView, "pages/sale/client-list", false); // Redirect to sales list page
     }
 }
