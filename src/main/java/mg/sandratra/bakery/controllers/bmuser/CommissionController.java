3ecd300426bdca670e3c4dfe7eecb1eb07d49721
sandratraniaina <sandratrarafa@gmail.com>
2025-01-21 14:59:09 +0300

    feat: Create CommissionController

---
+++ src/main/java/mg/sandratra/bakery/controllers/bmuser/CommissionController.java
@@ -0,0 +1,33 @@
+package mg.sandratra.bakery.controllers.bmuser;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import lombok.RequiredArgsConstructor;
+import mg.sandratra.bakery.controllers.BaseController;
+import mg.sandratra.bakery.models.bmuser.Commission;
+import mg.sandratra.bakery.services.bmuser.BmUserService;
+import mg.sandratra.bakery.services.bmuser.CommissionService;
+import mg.sandratra.bakery.services.bmuser.filter.CommissionFilter;
+
+@Controller
+@RequestMapping("/users/commissions")
+@RequiredArgsConstructor
+public class CommissionController extends BaseController {
+
+    private final CommissionService commissionService;
+    private final BmUserService bmUserService;
+
+    public ModelAndView commissionsPage(@ModelAttribute CommissionFilter filter) {
+        List<Commission> commissions = commissionService.search(filter);
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("filter", filter);
+        modelAndView.addObject("users", bmUserService.findAll());
+        modelAndView.addObject("commissions", commissions);
+        return redirect(modelAndView, "pages/user/commissions", false); // Redirect to user commissions page
+    }
+}
