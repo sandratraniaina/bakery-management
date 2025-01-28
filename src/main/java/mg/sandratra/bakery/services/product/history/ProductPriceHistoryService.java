9351451326ef6787eed7cabf3ffcf079c6605ef4
sandratraniaina <sandratrarafa@gmail.com>
2025-01-28 15:19:44 +0300

    feat: Create ProdutcPriceHistory method

---
+++ src/main/java/mg/sandratra/bakery/services/product/history/ProductPriceHistoryService.java
@@ -0,0 +1,49 @@
+package mg.sandratra.bakery.services.product.history;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import mg.sandratra.bakery.models.product.Product;
+import mg.sandratra.bakery.models.product.history.ProductPrice;
+import mg.sandratra.bakery.models.product.history.ProductPriceHistory;
+import mg.sandratra.bakery.services.product.ProductService;
+import mg.sandratra.bakery.services.product.filter.ProductPriceHistoryFilter;
+
+@Service
+@RequiredArgsConstructor
+public class ProductPriceHistoryService {
+
+    private final ProductService productService;
+    private final ProductPriceService productPriceService;
+
+    public ProductPriceHistory mapToProductPriceHistory(List<ProductPrice> productPrices) {
+        if (productPrices == null || productPrices.isEmpty()) {
+            throw new IllegalArgumentException("The product prices list cannot be null or empty");
+        }
+
+        Long productId = productPrices.get(0).getProduct().getId();
+        if (productId == null) {
+            throw new IllegalArgumentException("Product ID cannot be null in ProductPrice");
+        }
+
+        Product product = productService.findById(productId);
+        if (product == null) {
+            throw new IllegalArgumentException("Product not found for ID: " + productId);
+        }
+
+        // Set the Product object in each ProductPrice
+        List<ProductPrice> updatedProductPrices = productPrices.stream()
+                .peek(productPrice -> productPrice.setProduct(product))
+                .toList();
+
+        // Create and return the ProductPriceHistory object
+        return new ProductPriceHistory(product, updatedProductPrices);
+    }
+
+    public ProductPriceHistory fitlerHistory(ProductPriceHistoryFilter filter) {
+        List<ProductPrice> productPrices = productPriceService.filterProductPrices(filter);
+        return mapToProductPriceHistory(productPrices);
+    }
+}
