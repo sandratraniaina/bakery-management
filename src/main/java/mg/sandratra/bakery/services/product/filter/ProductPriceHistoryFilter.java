674827ba14fac3806c55af3b897df037c59bf850
sandratraniaina <sandratrarafa@gmail.com>
2025-01-29 20:10:48 +0300

    fix: Fix ProductPriceHistoryFilter for date filters

--- src/main/java/mg/sandratra/bakery/services/product/filter/ProductPriceHistoryFilter.java
+++ src/main/java/mg/sandratra/bakery/services/product/filter/ProductPriceHistoryFilter.java
@@ -4,6 +4,9 @@ import java.sql.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.util.StringUtils;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,6 +19,34 @@ public class ProductPriceHistoryFilter implements Filter {
     private Long productId;
     private Date maxDate;
     private Date minDate;
+
+    // Remove @DateTimeFormat from the fields and only use it in the setter methods
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    public void setMinDate(String minDate) {
+        if (StringUtils.hasText(minDate)) {
+            this.minDate = Date.valueOf(minDate);
+        } else {
+            this.minDate = null;
+        }
+    }
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    public void setMaxDate(String maxDate) {
+        if (StringUtils.hasText(maxDate)) {
+            this.maxDate = Date.valueOf(maxDate);
+        } else {
+            this.maxDate = null;
+        }
+    }
+
+    // Add getters that return the string representation when needed
+    public String getMinDate() {
+        return minDate != null ? minDate.toString() : null;
+    }
+
+    public String getMaxDate() {
+        return maxDate != null ? maxDate.toString() : null;
+    }
 
     @Override
     public String buildQuery() {
